585fbb5f11a0b068122f465bddad49c134daad88
dimitrycastex@gmail.com <dimitrycastex@gmail.com@d59c3b2b-69c4-2eff-f080-28d6b33c3df0>
2012-01-11 20:23:28 +0000

    TestPDF: clase testeadora de documentos Doc1: Imprimir Lista Clientes [Agregado] Doc2: Imprimir Lista Remates [Agregado]

---
+++ src/Impresion/TestPDF.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Impresion;
+
+import com.itextpdf.text.DocumentException;
+import java.io.FileNotFoundException;
+
+
+
+/**
+ *
+ * @author Dimitry
+ */
+public class TestPDF {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) throws DocumentException, FileNotFoundException
+    {
+        String path="D:/DB/";
+        Imprimir_Lista_Remates.imprimir(path);
+        Imprimir_Lista_Clientes.imprimir(path);
+    }
+
+}
