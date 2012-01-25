cba5f071875977c60b69852360daaa6d8a4d38f7
booscapi@gmail.com <booscapi@gmail.com@d59c3b2b-69c4-2eff-f080-28d6b33c3df0>
2012-01-25 07:04:23 +0000

    Se actualizo y testio la clase Imprimir_Caja_Remate

--- src/Impresion/Imprimir_Caja_Remate.java
+++ src/Impresion/Imprimir_Caja_Remate.java
@@ -9,8 +9,8 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-
-
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
@@ -18,11 +18,18 @@ import java.io.FileOutputStream;
  */
 public class Imprimir_Caja_Remate
 {
+    public static ArrayList remate;
+    public static ArrayList<ArrayList> factura;
     //@param ruta: ruta absoluta o relativa en donde crear el archivos
-    public static void imprimir(String ruta) throws DocumentException, FileNotFoundException
+    public static void imprimir(String ruta,String ID_Remate) throws DocumentException, FileNotFoundException
     {
       Document document = new Document();
       PdfWriter.getInstance(document,new FileOutputStream(ruta+"caja_de_remates.pdf"));
+      
+      remate = Modelo.Remate.getDatos(ID_Remate);
+      factura = Modelo.Remate.get_Lista_Caja_Remate(ID_Remate);
+      
+      
       //abrir el pdf
       document.open();
       //ir escribiendo en el pdf
@@ -32,20 +39,20 @@ public class Imprimir_Caja_Remate
       document.add(formato.titulo("CAJA DEL REMATE"));
       document.add(new Phrase(""));//espacio
       
-      document.add(formato.subtitulo("REMATE N°120 : JUDICIAL COQUIMBO"));
+      document.add(formato.subtitulo(remate.get(4).toString()));
       document.add(new Phrase(""));//espacio
       
-      document.add(formato.subtitulo("Comision : 10%"));
+      document.add(formato.subtitulo("Comision : "+remate.get(5).toString()+"%"));
       document.add(new Phrase("\n\n"));//espacio
       
       //Paragraph pp = new Paragraph("Dirección/Lugar",new Font(FontFamily.TIMES_ROMAN, 8, Font.BOLD));
       
       document.add(formato.texto_normal("Ciudad/Lugar : ",Font.BOLD));
-      document.add(formato.texto_normal("Municipalidad La Serena"));
+      document.add(formato.texto_normal(remate.get(6).toString()+"/"+remate.get(1)));
       document.add(new Phrase("\n"));//espacio
       
       document.add(formato.texto_normal("Fecha               : ",Font.BOLD));
-      document.add(formato.texto_normal("21/10/2005"));
+      document.add(formato.texto_normal(remate.get(3).toString()));
       
       document.add(CrearTabla());
       //cerrar el pdf
@@ -64,28 +71,80 @@ public class Imprimir_Caja_Remate
             // alineación de la tabla entera centrada
             table.setHorizontalAlignment(1);
             
+            /*
+              remate.add(rs.getInt("ID_FACTURA"));x
+              remate.add(rs.getLong("Total"));-
+              remate.add(rs.getLong("Garantia"));-
+              remate.add(rs.getDate("Fecha_Emision"));x
+              remate.add(rs.getString("Estado"));x
+              remate.add(rs.getString("Exento"));-
+              remate.add(rs.getLong("Neto"));x
+              remate.add(rs.getInt("Impuestos"));-
+              remate.add(rs.getInt("Comision"));  -
+              remate.add(rs.getInt("IVA"));-
+              remate.add(rs.getInt("comision_Factura"));-
+             */
+            
+            int TotalGarantias = 0;
+            int Impuesto = 0;
+            int FacturaIva = 0;
+            int TotalComision = 0;
+            int Total = 0;
+            
+            
+            for (Iterator it = factura.iterator(); it.hasNext();) // itera
+            {
+                ArrayList object = (ArrayList) it.next(); // castea
+                
+                //System.out.println("ID_FACTURA : "+object.get(0).toString());
+                //System.out.println("Fecha_Emision : "+object.get(3).toString());
+                //System.out.println("Estado : "+object.get(4));
+                
+                
+                //System.out.println("Impuestos : "+object.get(7).toString());
+                
+                
+                //System.out.println("Exento : "+object.get(5));
+                //System.out.println("Neto : "+Integer.parseInt(object.get(6).toString()));
+                //System.out.println("IVA : "+object.get(9).toString());
+                if (object.get(5).toString().equals("N")) {
+                    Impuesto = (int)(Float.parseFloat(object.get(6).toString())*(Float.parseFloat(object.get(9).toString())/100));
+                    //System.out.println("comision_Factura : "+object.get(10).toString());
+                    FacturaIva = FacturaIva + (int)(Impuesto*Float.parseFloat(object.get(10).toString()));
+                }
+                //System.out.println("comision_Factura : "+object.get(10).toString());
+                else FacturaIva = FacturaIva + (int)(Float.parseFloat(object.get(10).toString())*(Float.parseFloat(object.get(9).toString())/100));
+                
+                //System.out.println("comision_Factura : "+object.get(10).toString());
+                TotalComision = TotalComision + Integer.parseInt(object.get(10).toString());
+                
+                //System.out.println("Garantia :"+Integer.parseInt(object.get(2).toString()));
+                TotalGarantias = TotalGarantias + Integer.parseInt(object.get(2).toString());
+                //System.out.println("Total : "+Integer.parseInt(object.get(1).toString()));
+                Total = Total + Integer.parseInt(object.get(1).toString());
+            }
             
             // 
             table.addCell(formato.celda_normal("Total Garantias"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            table.addCell(formato.celda_normal("$"+Integer.toString(TotalGarantias),2,Element.ALIGN_RIGHT));
             
-            table.addCell(formato.celda_normal("Total Exento"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            //table.addCell(formato.celda_normal("Total Exento"));
+            //table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
             
-            table.addCell(formato.celda_normal("Comision"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            //table.addCell(formato.celda_normal("Comision"));
+            //table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
             
-            table.addCell(formato.celda_normal("Gastos"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            //table.addCell(formato.celda_normal("Gastos"));
+            //table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
             
             table.addCell(formato.celda_normal("Total Comision"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            table.addCell(formato.celda_normal("$"+Integer.toString(TotalComision),2,Element.ALIGN_RIGHT));
             
             table.addCell(formato.celda_normal("Total IVA"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            table.addCell(formato.celda_normal("$"+Integer.toString(FacturaIva),2,Element.ALIGN_RIGHT));
             
             table.addCell(formato.celda_normal("Total"));
-            table.addCell(formato.celda_normal("$234.465",2,Element.ALIGN_RIGHT));
+            table.addCell(formato.celda_normal("$"+Integer.toString(Total),2,Element.ALIGN_RIGHT));
             
             return table;
         }
